882c02fdfdac934a7510268c177c02dd3d25ef05
Trisha Gee <trisha.gee@gmail.com>
2018-07-30 17:57:22 +0200

    Simplify code

--- standard-java/src/main/java/com/jetbrains/editing/MultiLineStrings.java
+++ standard-java/src/main/java/com/jetbrains/editing/MultiLineStrings.java
@@ -10,7 +10,7 @@ public class MultiLineStrings {
 
         // Pressing Enter in String will auto-insert +
         // Ctrl+Shift+J will join lines
-        String someText = "This code can be on a single line " +
+        var someText = "This code can be on a single line " +
                 "or it can be on multiple lines " +
                 "and you can type and hit Enter and get the + sign " +
                 "inserted automatically";
